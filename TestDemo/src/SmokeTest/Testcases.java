c0bda1e0c97bc3535f0774322ed64b32aa3fde56
Yashwanth Nalla (Kaara Info systems Private Lim) <v-ynalla@microsoft.com>
2021-06-01 15:47:38 +0530

    adding class and jar file

--- TestDemo/src/SmokeTest/Testcases.java
+++ TestDemo/src/SmokeTest/Testcases.java
@@ -1,11 +1,16 @@
 package SmokeTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testcases {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	int a;
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\v-yasna\\git\\TestDemo\\TestDemo\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
 	}
 
 }
--- TestDemo/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module TestDemo {
-	exports SmokeTest;
-}
